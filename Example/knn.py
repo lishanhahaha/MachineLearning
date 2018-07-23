ad2e3e89a68e2cbbcd2dfe57d7f312c4b97cb7f6
lishanhahaha <lishanhahaha@163.com>
2018-07-23 17:54:57 +0800

    1.增加决策树最优算法计算

--- Example/knn.py
+++ Example/knn.py
@@ -1,143 +1,99 @@
-#-*-coding:utf-8-*-
+import matplotlib.pyplot as plt
 
-import os
-import time
-import operator
-
-import numpy as np
-from sklearn.neighbors import KNeighborsClassifier as kNN
+# 这里是对绘制是图形属性的一些定义，可以不用管，主要是后面的算法
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
 
 
-TEST_DIGITS = r'F:\MLiA_SourceCode\machinelearninginaction\Ch02\digits\testDigits'
-TRAIN_DIGITS = r'F:\MLiA_SourceCode\machinelearninginaction\Ch02\digits\trainingDigits'
-
-def createDataSetAndLabels():
-    ''' 创建数据集及相应的标签
-
-    Parameters:
-
-    Returns
-        dataSet:        创建的数据集
-        labels:         创建的数据集对应的标签
-    Modify:
-        2018-07-06
-    '''
-    dataSet = np.array([[1.0,1.0], [1.0,1.1], [0,0], [0,0.1]])
-    labels = np.array(['A', 'A', 'B', 'B'])
-    return dataSet, labels
-
-def classifyKNN(testData, dataSet, labels, k):
-    ''' kNN算法分类器
-
-    Parameters:
-        testData        -   用于分类的测试集
-        dataSet         -   用于训练的数据集
-        labels          -   分类标签
-        k               -   kNN算法参数，选择距离最小的k个点
-    Returns:
-        classResult[0][0]     -   分类结果
-    Modify:
-        2018-07-06
-    '''
-    # 计算距离
-    row = dataSet.shape[0]
-    diffMat = np.tile(testData, (row,1)) - dataSet
-    distances = ( (diffMat ** 2).sum(axis=1)) ** 0.5
-    # 计算最小距离
-    classCount = {}
-    sortedDistIndex = distances.argsort()       # 计算升序所对应的索引号
-    for i in range(k):
-        lbl = labels[sortedDistIndex[i]]
-        classCount[lbl] = classCount.get(lbl, 0) + 1
-    # 计算降序并分类出结果
-    classResult = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
-    return classResult[0][0]
-
-def file2matrix(filename):
-    ''' 用于将文件中的手写数字集转换成矩阵
-
-    Parameters:
-        filename            -   文件路径
-    Returns:
-        dataSet             -   返回文件里面的矩阵数据集
-    Modify:
-        2018-07-06
-    '''
-    f = open(filename)
-    dataSet = np.zeros((1, 1024))
-    for i in range(32):
-        data = f.readline()
-        for j in range(32):
-            dataSet[0,32*i+j] = int(data[j])
-    f.close()
-    return dataSet
-
-def handwriteClassTest():
-    ''' 用于测试手写分类数字
-    Parameters:
-
-    Returns:
-
-    Modify:
-        2018-07-06
-    '''
-    # 训练数据集
-    listTrainFile = os.listdir(TRAIN_DIGITS)
-    numTrainFile = len(listTrainFile)
-    trainLabels = []
-    trainDataSet = np.zeros((numTrainFile,1024))
-    for i in range(numTrainFile):
-        lbl = listTrainFile[i].split('_')
-        trainLabels.append(int(lbl[0]))
-        trainDataSet[i,:] = file2matrix(TRAIN_DIGITS+os.sep+listTrainFile[i])
-    # 测试训练集
-    listTestFile = os.listdir(TEST_DIGITS)
-    numTestFile = len(listTestFile)
-    errorCount = 0
-    for i in range(numTestFile):
-        lbl = listTestFile[i].split('_')
-        realResult = int(lbl[0])
-        testDataSet = file2matrix(TEST_DIGITS+os.sep+listTestFile[i])
-        testResult = int(classifyKNN(testDataSet, trainDataSet, trainLabels, 3))
-        print('真实结果：%s      分类结果：%s' % (realResult, testResult))
-        if (realResult != testResult):
-            errorCount += 1
-    errorRatio = round(errorCount/numTestFile*100, 2)
-    print('错误率：%s %%' % (errorRatio), errorCount, numTestFile, type(errorCount), type(numTestFile))
-
-def handwriteClassKNN():
-    trainFileList = os.listdir(TRAIN_DIGITS)
-    trainFileNum = len(trainFileList)
-    labels = []
-    trainDataSet = np.zeros((trainFileNum, 1024))
-    for i in range(trainFileNum):
-        lbl = trainFileList[i].split('_')
-        labels.append(int(lbl[0]))
-        trainDataSet[i,:] = file2matrix(TRAIN_DIGITS+os.sep+trainFileList[i])
-    # 运用kNN算法进行分类
-    testFileList = os.listdir(TEST_DIGITS)
-    testFileNum = len(testFileList)
-    neigh = kNN(n_neighbors=3, weights='uniform', algorithm='auto')
-    neigh.fit(trainDataSet, labels)
-    errorCount = 0
-    for i in range(testFileNum):
-        lbl = testFileList[i].split('_')
-        realResult = int(lbl[0])
-        testDataSet = file2matrix(TEST_DIGITS+os.sep+testFileList[i])
-        classResult = int(neigh.predict(testDataSet))
-        print('真实值：%s       预测值：%s' % (realResult, classResult))
-        if (realResult != classResult):
-            errorCount += 1
-    errorRatio = errorCount/testFileNum*100
-    print('错误率：%s %%' % (errorRatio))
+# 这是递归计算树的叶子节点个数，比较简单
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = next(iter(myTree))
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[
+                    key]).__name__ == 'dict':  # test to see if the nodes are dictonaires, if not they are leaf nodes
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
 
 
-if __name__ == '__main__':
-    startTime = time.time()
-    '''dataSet, labels = createDataSetAndLabels()
-    classResult = classifyKNN([1,0.1], dataSet, labels, 2)
-    print(classResult)'''
-    #handwriteClassTest()
-    handwriteClassKNN()
-    endTime = time.time()
-    print('测试时间：%s s' % (endTime - startTime))
+# 这是递归计算树的深度，比较简单
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = next(iter(myTree))
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[
+                    key]).__name__ == 'dict':  # test to see if the nodes are dictonaires, if not they are leaf nodes
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth
+    return maxDepth
+
+
+# 这个是用来一注释形式绘制节点和箭头线，可以不用管
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
+                            xytext=centerPt, textcoords='axes fraction',
+                            va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
+
+
+# 这个是用来绘制线上的标注，简单
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+
+
+# 重点，递归，决定整个树图的绘制，难（自己认为）
+def plotTree(myTree, parentPt, nodeTxt):  # if the first key tells you what feat was split on
+    numLeafs = getNumLeafs(myTree)  # this determines the x width of this tree
+    depth = getTreeDepth(myTree)
+    firstStr = next(iter(myTree))  # the text label for this node should be this
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)
+
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[
+                    key]).__name__ == 'dict':  # test to see if the nodes are dictonaires, if not they are leaf nodes
+            plotTree(secondDict[key], cntrPt, str(key))  # recursion
+        else:  # it's a leaf node print the leaf node
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+
+
+# if you do get a dictonary you know it's a tree, and the first element will be another dict
+
+# 这个是真正的绘制，上边是逻辑的绘制
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False)  # no ticks
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5 / plotTree.totalW;
+    plotTree.yOff = 1.0;
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.show()
+
+
+# 这个是用来创建数据集即决策树
+def retrieveTree(i):
+    listOfTrees = [{'no surfacing': {0: {'flippers': {0: 'no', 1: 'yes'}}, 1: {'flippers': {0: 'no', 1: 'yes'}},
+                                     2: {'flippers': {0: 'no', 1: 'yes'}}}},
+                   {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
+                   ]
+    return listOfTrees[i]
+
+
+createPlot(retrieveTree(1))
--- Example/tree.py
+++ Example/tree.py
@@ -1,4 +1,125 @@
+# -*- coding:utf-8 -*-
+# !/usr/bin/env python3
+import operator
+import json
+import math
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
 
+class DecisionTree(object):
+    def __init__(self):
+        pass
 
+    def createDataSet(self):
+        ''' 创建数据集
+
+        Paraments:
+
+        Returns:
+            dataSet                 -   生成的数据集
+            labels                  -   数据集对应的标签
+        Modify:
+            2018-07-23
+        Authon:
+            Li Shan
+        '''
+        dataSet = [[0, 0, 0, 0, 'no'],
+                   [0, 0, 0, 1, 'no'],
+                   [0, 1, 0, 1, 'yes'],
+                   [0, 1, 1, 0, 'yes'],
+                   [0, 0, 0, 0, 'no'],
+                   [1, 0, 0, 0, 'no'],
+                   [1, 0, 0, 1, 'no'],
+                   [1, 1, 1, 1, 'yes'],
+                   [1, 0, 1, 2, 'yes'],
+                   [1, 0, 1, 2, 'yes'],
+                   [2, 0, 1, 2, 'yes'],
+                   [2, 0, 1, 1, 'yes'],
+                   [2, 1, 0, 1, 'yes'],
+                   [2, 1, 0, 2, 'yes'],
+                   [2, 0, 0, 0, 'no']]
+        labels = ['年龄', '有工作', '有自己的房子', '信贷情况']        #特征标签
+        return dataSet, labels
+
+    def splitDataSet(self, dataSet, index, feature):
+        ''' 分割数据集
+        Paraments:
+            dataSet                         -   输入待分割的数据集
+        Returns:
+            subDataSet                      -   分割成功的数据集
+        Modify:
+            2018-07-23
+         Author:
+            Li Shan
+        '''
+        subDataSet = []
+        for data in dataSet:
+            if data[index] == feature:
+                d = data[:index] + data[index+1:]
+                subDataSet.append(d)
+        return subDataSet
+
+    def chooseBestFeature(self, dataSet):
+        ''' 选择最优特征
+
+        Paraments:
+            dataSet                     -   输入的数据集
+        Returns:
+            bestFeature                 -   最优特征的索引值
+        Modify:
+            2018-0723
+        Author:
+            Li Shan
+        '''
+        featureCount = len(dataSet[0]) - 1
+        baseShannonEntropy = self.calcShannonEntropy(dataSet)
+        infoGain = {}
+        for featureIndex in range(featureCount):
+            featureLists = [example[featureIndex] for example in dataSet]
+            featureUnique = set(featureLists)
+            conditionEntropy = 0.0
+            for feature in featureUnique:
+                subDataSet = self.splitDataSet(dataSet, featureIndex, feature)
+                prob = len(subDataSet) / len(featureLists)
+                conditionEntropy += prob * self.calcShannonEntropy(subDataSet)
+            infoGain[featureIndex] = baseShannonEntropy - conditionEntropy
+        infoGain = sorted(infoGain.items(), key=operator.itemgetter(1), reverse=True)
+        return infoGain[0][0]
+
+    def calcShannonEntropy(self, dataSet):
+        ''' 计算经验熵
+
+        Paraments:
+            dataSet                     -   输入的数据集
+        Returns:
+
+        Modefy:
+            2018-07-23
+        Authon:
+            Li Shan
+        '''
+        resultClass = {}
+        resultCount = len(dataSet)
+        shannonEntropy = 0.0
+        for data in dataSet:
+            label = data[-1]
+            resultClass[label] = resultClass.get(label, 0) + 1
+        for k,v in resultClass.items():
+            prob = v / resultCount
+            shannonEntropy -= prob * math.log(prob, 2)
+        return shannonEntropy
+
+    def createTree(self, dataSet, labels):
+        bestFeature = self.chooseBestFeature(dataSet)
+        key = labels[bestFeature]
+        myTree = {key:{}}
+
+        print(myTree)
+
+if __name__ == '__main__':
+    dt = DecisionTree()
+    dataSet, labels = dt.createDataSet()
+    bestFeature = dt.chooseBestFeature(dataSet)
+    myTree = dt.createTree(dataSet, labels)
